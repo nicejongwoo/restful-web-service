a120f5e4677d192d89c1adfd790b114674f7cd1c
jongwoo <leejongwoo.dev@gmail.com>
2021-10-01 23:46:31 +0900

    user domain and service

---
+++ src/main/java/com/example/rws/user/UserDaoService.java
@@ -0,0 +1,39 @@
+package com.example.rws.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "user1", new Date()));
+        users.add(new User(2, "user2", new Date()));
+        users.add(new User(3, "user3", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+}
