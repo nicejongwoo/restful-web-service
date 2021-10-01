77bc6784bc4d51d74e2da359059f4d2685f093fd
jongwoo <leejongwoo.dev@gmail.com>
2021-10-02 00:11:45 +0900

    GET HTTP Method

---
+++ src/main/java/com/example/rws/user/UserController.java
@@ -0,0 +1,26 @@
+package com.example.rws.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+}
