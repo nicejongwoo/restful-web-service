a1c259eda83d1173711341b8f4047963d5c85040
jongwoo <leejongwoo.dev@gmail.com>
2021-10-04 14:19:53 +0900

    UserJPAController, UserRepository

---
+++ src/main/java/com/example/rws/user/UserJPAController.java
@@ -0,0 +1,36 @@
+package com.example.rws.user;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jpa")
+public class UserJPAController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public MappingJacksonValue userList() {
+        List<User> users = userRepository.findAll();
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+}
