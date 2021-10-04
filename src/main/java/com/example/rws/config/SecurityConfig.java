d9318f0b89de527b8d6d70ab0829f7adf061df67
jongwoo <leejongwoo.dev@gmail.com>
2021-10-04 12:15:23 +0900

    jpa, h2 dependency, h2 console setting

--- src/main/java/com/example/rws/config/SecurityConfig.java
+++ src/main/java/com/example/rws/config/SecurityConfig.java
@@ -2,12 +2,23 @@ package com.example.rws.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //h2 DB console 사용을 위해
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("!/h2-console/**"));
+        http.headers().frameOptions().disable(); //X-Frame-Options in Spring Security 중지
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
